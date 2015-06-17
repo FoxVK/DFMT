667933d5d179f74a9ce2ef647d37d626d79880e1
FoxVK <fox@vsetin.org>
2015-06-17 22:43:07 +0200

    Added debughalt()

--- firmware/drv/utils.h
+++ firmware/drv/utils.h
@@ -13,5 +13,7 @@ void uint2hex(unsigned number, char* buf, size_t length);
 unsigned virt2phy(void* virt_addr);
 void* phy2virt(unsigned phy_addr);
 
+#define debughalt() __asm__ volatile (" sdbbp 0")
+
 #endif	/* UTILS_H */
 
