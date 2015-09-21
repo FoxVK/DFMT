31404290ab6b21b9cad594dfb834cbd14c889ceb
FoxVK <fox@vsetin.org>
2015-09-21 16:14:04 +0200

    Added Exception catching function with debug_uart output

--- firmware/main.c
+++ firmware/main.c
@@ -20,6 +20,8 @@
 #include <xc.h>
 #include "app_config.h"
 #include "app.h"
+#include "drv/debug_uart.h"
+#include "drv/utils.h"
 
 
 
@@ -53,35 +55,37 @@ void main(void)
 
 
 
-/*
+
 void _general_exception_handler (unsigned cause, unsigned status)
 {
     static unsigned int _excep_addr = 0;
     asm volatile("mfc0 %0,$14" : "=r" (_excep_addr));
 
-    char str[30];
-    sprintf(str, "\r\n Exception @0x%x: ", _excep_addr);
-    SYS_DEBUG_Message(str);
+    char str[]="exception @ addr xxxxxx";
+    uint2hex( _excep_addr, str+17, 6);
+    str[23]=0;
+    debug_uart_write(str);
 
     switch((cause & 0x7c)>>2)
     {
-        case 0 : SYS_DEBUG_Message("interrupt"); break;
-        case 4 : SYS_DEBUG_Message("address error exception (load or ifetch)"); break;
-        case 5 : SYS_DEBUG_Message("address error exception (store)"); break;
-        case 6 : SYS_DEBUG_Message("bus error (ifetch)"); break;
-        case 7 : SYS_DEBUG_Message("bus error (load/store)"); break;
-        case 8 : SYS_DEBUG_Message("syscall"); break;
-        case 9 : SYS_DEBUG_Message("breakpoint"); break;
-        case 10: SYS_DEBUG_Message("reserved instruction"); break;
-        case 11: SYS_DEBUG_Message("coprocessor unusable"); break;
-        case 12: SYS_DEBUG_Message("arithmetic overflow"); break;
-        case 13: SYS_DEBUG_Message("trap (possible divide by zero)"); break;
-        case 16: SYS_DEBUG_Message("implementation specfic 1"); break;
-        case 17: SYS_DEBUG_Message("CorExtend Unuseable"); break;
-        case 18: SYS_DEBUG_Message("coprocessor 2"); break;
-        default: SYS_DEBUG_Message("Unknown reason"); break;
+        case 0 : debug_uart_write("interrupt"); break;
+        case 4 : debug_uart_write("address error exception (load or ifetch)"); break;
+        case 5 : debug_uart_write("address error exception (store)"); break;
+        case 6 : debug_uart_write("bus error (ifetch)"); break;
+        case 7 : debug_uart_write("bus error (load/store)"); break;
+        case 8 : debug_uart_write("syscall"); break;
+        case 9 : debug_uart_write("breakpoint"); break;
+        case 10: debug_uart_write("reserved instruction"); break;
+        case 11: debug_uart_write("coprocessor unusable"); break;
+        case 12: debug_uart_write("arithmetic overflow"); break;
+        case 13: debug_uart_write("trap (possible divide by zero)"); break;
+        case 16: debug_uart_write("implementation specfic 1"); break;
+        case 17: debug_uart_write("CorExtend Unuseable"); break;
+        case 18: debug_uart_write("coprocessor 2"); break;
+        default: debug_uart_write("Unknown reason"); break;
     }
 
     _nop();
-    while(true);
-}*/
+    while(true)
+        debug_uart_task();
+}
